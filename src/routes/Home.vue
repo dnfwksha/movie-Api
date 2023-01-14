739fa8b567bf3a4a33c012ba2c3f473d1eb34356
Juhwan <dnfwksha@gmail.com>
2023-01-14 23:51:41 +0900

    Initial commit

---
+++ src/routes/Home.vue
@@ -0,0 +1,19 @@
+<template>
+<Headline/>
+  <Search/>
+</template>
+
+<script>
+import Headline from "@/components/Headline.vue";
+import Search from "@/components/Search.vue";
+export default {
+  components:{
+    Headline,
+    Search
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
